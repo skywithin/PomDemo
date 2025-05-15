e7e418a5327dd4827dc39e681cc713ec0b8d460c
Max Vagner <sydneymax@gmail.com>
2025-05-15 13:18:26 +1000

    Add "Cities" buttons

---
+++ PomDemo/Models/CitiesRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace PomDemo.Models;
+
+public static class CitiesRepository
+{
+    private static List<string> cities =
+        new()
+        {
+            "Toronto",
+            "Montreal",
+            "Ottawa",
+            "Calgary",
+            "Halifax"
+        };
+
+    public static IEnumerable<string> GetCities() => cities;
+}
